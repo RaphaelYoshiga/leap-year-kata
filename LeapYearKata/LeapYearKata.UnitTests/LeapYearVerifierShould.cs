8d292883ef7aa0502540425deae0e1be9927d180
Jonathan Povey <jonathan.povey@asos.com>
2016-12-07 10:40:54 +0000

    Initial commit

---
+++ LeapYearKata/LeapYearKata.UnitTests/LeapYearVerifierShould.cs
@@ -0,0 +1,17 @@
+﻿namespace LeapYearKata.UnitTests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LeapYearVerifierShould
+    {
+        [Test]
+        public void Return_True_When_Input_Is_4()
+        {
+            var result = LeapYearVerifier.IsLeapYear(4);
+
+            result.Should().BeTrue();
+        }
+    }
+}
---
+++ LeapYearKata/LeapYearKata/LeapYearVerifier.cs
@@ -0,0 +1,10 @@
+namespace LeapYearKata
+{
+    public static class LeapYearVerifier
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return true;
+        }
+    }
+}
